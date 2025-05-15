67c7bc3beab8e6ab427be9b12d3c19407eaeb2d7
kiech714 <kiech1256@gmail.com>
2025-05-15 15:18:40 +0200

    Poprawiony db_config.py

--- db_config.py
+++ db_config.py
@@ -2,13 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-postgresql://fastapi_db_hfiw_user:GytQ6ghlrRU6iOMNJeQYq6KUdN9Ktps0@dpg-d0itovre5dus739vaccg-a.frankfurt-postgres.render.com/fastapi_db_hfiw
+# Twój connection string z Rendera (upewnij się, że go nie trzymasz publicznie w produkcji)
+DATABASE_URL = "postgresql://fastapi_db_hfiw_user:GytQ6ghlrRU6iOMNJeQYq6KUdN9Ktps0@dpg-d0itovre5dus739vaccg-a.frankfurt-postgres.render.com/fastapi_db_hfiw"
 
-
+# Tworzenie silnika bazy danych
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+# Utworzenie sesji i bazy
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Funkcja zależności do FastAPI
 def get_db():
     db = SessionLocal()
     try:
